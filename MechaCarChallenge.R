6c45fb1dac529c100a8a184530367f92b296729b
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-13 21:09:14 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,17 @@
 library(dplyr)
 
+
+## MechaCar Linear Regression 
 MechaCar <- read.csv("MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
 
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =MechaCar))
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =MechaCar))
+
+##Suspension Coil Visualizations 
+sus_coil <- read.csv("Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+total_summary <- sus_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+lot_summary <- sus_coil %>% group_by(sus_coil$Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
