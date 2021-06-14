175a8997230340584752ba85348268665dbad227
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-13 20:20:43 -0500

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+MechaCar <- read.csv("MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data =MechaCar))
